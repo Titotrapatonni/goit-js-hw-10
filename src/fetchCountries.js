f2f8b68aff81a54c9b46326fe1917ce60234f93f
Titotrapatonni <titovtelexfree@gmail.com>
2023-01-05 17:01:08 +0200

    fixed after review

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ export function fetchCountries(name) {
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(resp => {
     if (!resp.ok) {
-      throw new Error();
+      throw new Error(resp.statusText);
     }
     return resp.json();
   });
--- src/index.js
+++ src/index.js
@@ -7,22 +7,28 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('input#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-let markup = '';
 input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function onSearch() {
-  markup = '';
+function resetMarkup() {
   countryList.innerHTML = '';
   countryInfo.innerHTML = '';
-  if (input.value.trim()) {
-    fetchCountries(input.value.trim())
-      .then(countries => markupCountries(countries))
-      .catch(err => Notify.failure('Oops, there is no country with that name'));
+}
+
+function onSearch() {
+  resetMarkup();
+  const value = input.value.trim();
+  if (!value) {
+    return;
   }
+  fetchCountries(value)
+    .then(countries => markupCountries(countries))
+    .catch(err => Notify.failure('Oops, there is no country with that name'));
 }
+
 function createCountryListMarkup({ name, flags }) {
-  markup += `<li class="country-item"><img src="${flags.svg}" alt="прапор ${name.official}" width="20"><h2 class="country-name">${name.common}</h2></li>`;
+  return `<li class="country-item"><img src="${flags.svg}" alt="прапор ${name.official}" width="20"><h2 class="country-name">${name.common}</h2></li>`;
 }
+
 function createCountryInfoMarkup({
   name,
   capital,
@@ -30,21 +36,22 @@ function createCountryInfoMarkup({
   flags,
   languages,
 }) {
-  markup = `<div class="country-title">
+  return `<div class="country-title">
   <img src="${flags.svg}" alt="Прапор ${name.official}" width="30"/>
      <h2>${name.common}</h2></div>
      <p><b>Capital: </b> ${capital}</p>
      <p><b>Popualtion: </b> ${population}</p>
      <p><b>Languages: </b> ${Object.values(languages).join(', ')}</p>`;
 }
+
 function markupCountries(countries) {
   if (countries.length > 10) {
     Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (countries.length === 1) {
-    countries.map(country => createCountryInfoMarkup(country));
+    const markup = countries.map(country => createCountryInfoMarkup(country));
     countryInfo.innerHTML = markup;
   } else {
-    countries.map(country => createCountryListMarkup(country));
-    countryList.innerHTML = markup;
+    const markup = countries.map(country => createCountryListMarkup(country));
+    countryList.innerHTML = markup.join('');
   }
 }
